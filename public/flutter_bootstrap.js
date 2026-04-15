aff9d9432c2e1a3b2718aa0c279903553f5c541c
Kurnia Wahyu Adi <pt.ayotiket@gmail.com>
2026-04-16 06:38:32 +0700

    Perbarui web flutter

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "286030631" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2075810131" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
